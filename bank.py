2b998905f2e1c7d367bf48e28ea31a5e94bb5ad4
shubham-affinsys <shubham.chuahan@affinssys.com>
2024-08-09 15:10:47 +0530

    back to json data type

--- bank.py
+++ bank.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
-from typing import Optional
 from fastapi.middleware.cors import CORSMiddleware
-from data import accounts,users_by_id,phone_to_user_id,account_to_user_id,cif_to_user_id
+from pydantic import BaseModel
+
+from data import accounts, users_by_id
 
 #deployed on https://acc-balance.vercel.app/
 app = FastAPI()
@@ -15,11 +15,13 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 # In-memory data store
 
 
 class Account(BaseModel):
     balance: float
+
 
 @app.get("/accounts")
 def get_all_accounts():
@@ -29,6 +31,7 @@ def get_all_accounts():
         ussr = users_by_id[usr]
         all_accs.append(ussr["accounts"])
     return all_accs
+
 
 @app.get("/users")
 def get_all_users():
@@ -40,10 +43,11 @@ def get_all_users():
 
 
 @app.get("/users/{user_id}")
-def get_user(user_id:str):
-    if user_id in users_by_id.keys():    
+def get_user(user_id: str):
+    if user_id in users_by_id.keys():
         return users_by_id[user_id]
-    raise HTTPException(status_code=404,detail="user not found")
+    raise HTTPException(status_code=404, detail="user not found")
+
 
 @app.get("/accounts/{account_number}")
 def get_balance(account_number: str):
@@ -53,7 +57,6 @@ def get_balance(account_number: str):
     raise HTTPException(status_code=404, detail="Account not found")
 
 
-
 # create new account not using it
 @app.post("/accounts/{account_number}")
 def create_account(account_number: str, account: Account):
@@ -61,7 +64,7 @@ def create_account(account_number: str, account: Account):
         raise HTTPException(status_code=400, detail="Account already exists")
     new_account = {
         "title": f"101xxxx{account_number[-4:]}",
-        "account_no": account_number,  
+        "account_no": account_number,
         "balance": account.balance,
         "payload": account_number,
         "type": "payload"
@@ -69,6 +72,8 @@ def create_account(account_number: str, account: Account):
     accounts.append(new_account)
     return {"message": "Account created", "account_number": account_number, "balance": account.balance}
 
+
 if __name__ == '__main__':
     import uvicorn
+
     uvicorn.run(app, host="127.0.0.1", port=8000)
